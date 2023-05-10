a673840553a56eb8cf40061d349469470bbe2c86
PT-Jun0915 <parktaejun0915@naver.com>
2023-05-10 14:10:27 +0900

    1st video.end
    B38 video.end ( 38 : 55 )

--- Assets/Script/player.cs
+++ Assets/Script/player.cs
@@ -9,13 +9,15 @@ public class player : MonoBehaviour
     public float speed;
     float hAxis;
     float vAxis;
+    bool wDown;
 
     Vector3 moveVec;
+    Animator anim;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -23,9 +25,19 @@ public class player : MonoBehaviour
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk");
 
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-        transform.position += moveVec * speed * Time.deltaTime;
+        if(wDown) transform.position += moveVec * speed * 0.3f * Time.deltaTime;
+        else transform.position += moveVec * speed * Time.deltaTime;
+
+        anim.SetBool("Is_run", moveVec !=  Vector3.zero);
+        anim.SetBool("Is_walk", wDown);
+
+        //회전하는 방법
+        transform.LookAt(transform.position + moveVec);
+
     }
+
 }
